46e89735124a3f3c7ed585bfee5b06f557de35d6
Davidfhk <davidign1688@hotmail.com>
2017-12-08 20:53:48 +0100

    Nuestro index

---
+++ App/layndon/templates/index.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="../css/index.css">
+	<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js"></script>
+</head>
+<body>
+	<header>
+		
+	</header>
+	<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+	  <a class="navbar-brand" href="#">LAYNDON</a>
+	  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+	    <span class="navbar-toggler-icon"></span>
+	  </button>
+	  <div class="navbar-collapse collapse" id="navbarNav">
+	    <ul class="navbar-nav">
+	      <li class="nav-item">
+	        <a class="nav-link" href="#">PELICULAS<span class="sr-only">(current)</span></a>
+	      </li>
+	      <li class="nav-item">
+	        <a class="nav-link" href="#">VOTA</a>
+	      </li>
+	      <li class="nav-item">
+	        <a class="nav-link" href="#">CONTACTO</a>
+	      </li>
+	    </ul>
+	    <ul class="nav navbar-nav ml-auto">
+	    	<li class="nav-item">
+	    		<a class="nav-link" href="#">ENTRAR<span class="sr-only">(current)</span></a>
+	    	</li>
+	    	<li class="nav-item">
+	    		<a class="nav-link" href="#">REGISTRO<span class="sr-only">(current)</span></a>
+	    	</li>
+	    </ul>
+	    
+	  </div>
+	</nav>
+	<footer>
+		<p>Creado por David Ignacio Martos</p>
+	</footer>
+</body>
+</html>
