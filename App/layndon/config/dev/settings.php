fea3fb761fc3a3c9b2e5e1193ce80d1ba3cf1ea8
Davidfhk <davidign1688@hotmail.com>
2017-12-17 21:09:25 +0100

    añadimos las settings dentro de la carpeta de desarrollo

---
+++ App/layndon/config/dev/settings.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+
+	"db" => [
+	    'driver' => 'mysql',
+	    'host' => 'localhost',
+	    'database' => 'layndon-v2',
+	    'username' => 'root',
+	    'password' => '',
+	    'charset' => 'utf8',
+	    'prefix' => ''
+	]
+];
