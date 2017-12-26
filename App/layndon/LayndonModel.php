da6e1cd771a98f249b789e346b7034f05f0c14a3
Davidfhk <davidign1688@hotmail.com>
2017-12-27 00:06:05 +0100

    Añadimos el parametro de $omdb en el constructor Modificamos/añadimos el método setFilm Modificamos el método getFilms Añadimos el método deleteFilm

--- App/layndon/LayndonModel.php
+++ App/layndon/LayndonModel.php
@@ -5,19 +5,37 @@ namespace App\layndon;
 class LayndonModel
 {
 	private $queries;
+	private $omdb;
 
-	public function __construct ($queries)
+	public function __construct ($queries, $omdb)
 	{
 		$this->queries = $queries;
+		$this->omdb = $omdb;
 	}
 
-	public function setFilm ($film)
+	public function checkLogin ()
 	{
+		if($_POST['email'] == 'admin@admin.com' && $_POST['pass'] == '1234')
+		{
+			return 'admin';
+		}
+	}
+
+	public function setFilm ($name)
+	{
+		$film = $this->omdb->searchFilm($name);
+		
 		$this->queries->setFilm($film);
+
 	}
 
 	public function getFilms ()
 	{
-		$this->queries->getFilms();
+		return $this->queries->getFilms();
+	}
+
+	public function deleteFilm ($id)
+	{
+		$this->queries->deleteFilm($id);
 	}
 }
