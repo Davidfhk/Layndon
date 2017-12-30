8ab7afd8bca6f0ceb21df5610047283e13a1827a
Davidfhk <davidign1688@hotmail.com>
2017-12-30 11:35:36 +0100

    añadida la columna date, en la cual se registrara la fecha de exposición de la pelicula

--- table.sql
+++ table.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 26-12-2017 a las 23:50:09
+-- Tiempo de generación: 30-12-2017 a las 11:32:10
 -- Versión del servidor: 10.1.21-MariaDB
 -- Versión de PHP: 7.1.1
 
@@ -38,17 +38,17 @@ CREATE TABLE `films` (
   `country` varchar(50) NOT NULL,
   `awards` varchar(200) NOT NULL,
   `plot` text NOT NULL,
-  `image` varchar(500) NOT NULL
+  `image` varchar(500) NOT NULL,
+  `date` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- Volcado de datos para la tabla `films`
 --
 
-INSERT INTO `films` (`id_film`, `title`, `year`, `runtime`, `genre`, `director`, `actors`, `language`, `country`, `awards`, `plot`, `image`) VALUES
-(14, 'The Avengers', 2012, '143 min', 'Action, Adventure, Sci-Fi', 'Joss Whedon', 'Robert Downey Jr., Chris Evans, Mark Ruffalo, Chris Hemsworth', 'English, Russian, Hindi', 'USA', 'Nominated for 1 Oscar. Another 38 wins & 79 nominations.', 'Earth\'s mightiest heroes must come together and learn to fight as a team if they are going to stop the mischievous Loki and his alien army from enslaving humanity.', 'https://images-na.ssl-images-amazon.com/images/M/MV5BMTk2NTI1MTU4N15BMl5BanBnXkFtZTcwODg0OTY0Nw@@._V1_SX300.jpg'),
-(15, 'Spider Man', 1978, '30 min', 'Action, Adventure, Family', 'N/A', 'Shinji Tôdô, Mitsuo Andô, Yukie Kagawa, Hirofumi Koga', 'Japanese', 'Japan, USA', 'N/A', 'To fight against the evil Iron Cross Army, led by the space emperor Professor Monster, a daredevil motorcyclist transforms into the famous Marvel Superhero, with a racecar and giant ...', 'https://images-na.ssl-images-amazon.com/images/M/MV5BM2EwYzA2YjMtNDdhYi00OTI1LWE2ODUtOWViODk4YjRjNzVmXkEyXkFqcGdeQXVyNTAyODkwOQ@@._V1_SX300.jpg'),
-(17, 'Ice Age', 2002, '81 min', 'Animation, Adventure, Comedy', 'Chris Wedge, Carlos Saldanha(c', 'Ray Romano, John Leguizamo, Denis Leary, Goran Visnjic', 'English', 'USA', 'Nominated for 1 Oscar. Another 5 wins & 29 nominations.', 'Set during the Ice Age, a sabertooth tiger, a sloth, and a wooly mammoth find a lost human infant, and they try to return him to his tribe.', 'https://images-na.ssl-images-amazon.com/images/M/MV5BMmYxZWY2NzgtYzJjZC00MDFmLTgxZTctMjRiYjdjY2FhODg3XkEyXkFqcGdeQXVyNjk1Njg5NTA@._V1_SX300.jpg');
+INSERT INTO `films` (`id_film`, `title`, `year`, `runtime`, `genre`, `director`, `actors`, `language`, `country`, `awards`, `plot`, `image`, `date`) VALUES
+(27, 'Spider Man', 1978, '30 min', 'Action, Adventure, Family', 'N/A', 'Shinji Tôdô, Mitsuo Andô, Yukie Kagawa, Hirofumi Koga', 'Japanese', 'Japan, USA', 'N/A', 'To fight against the evil Iron Cross Army, led by the space emperor Professor Monster, a daredevil motorcyclist transforms into the famous Marvel Superhero, with a racecar and giant ...', 'https://images-na.ssl-images-amazon.com/images/M/MV5BM2EwYzA2YjMtNDdhYi00OTI1LWE2ODUtOWViODk4YjRjNzVmXkEyXkFqcGdeQXVyNTAyODkwOQ@@._V1_SX300.jpg', '25 de Enero'),
+(28, 'The Avengers', 2012, '143 min', 'Action, Adventure, Sci-Fi', 'Joss Whedon', 'Robert Downey Jr., Chris Evans, Mark Ruffalo, Chris Hemsworth', 'English, Russian, Hindi', 'USA', 'Nominated for 1 Oscar. Another 38 wins & 79 nominations.', 'Earth\'s mightiest heroes must come together and learn to fight as a team if they are going to stop the mischievous Loki and his alien army from enslaving humanity.', 'https://images-na.ssl-images-amazon.com/images/M/MV5BMTk2NTI1MTU4N15BMl5BanBnXkFtZTcwODg0OTY0Nw@@._V1_SX300.jpg', 'PROXIMAMENTE');
 
 --
 -- Índices para tablas volcadas
@@ -68,7 +68,7 @@ ALTER TABLE `films`
 -- AUTO_INCREMENT de la tabla `films`
 --
 ALTER TABLE `films`
-  MODIFY `id_film` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
+  MODIFY `id_film` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=29;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
