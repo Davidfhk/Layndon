54b42ba684176884d4e61500939ec6b46e56c58e
Davidfhk <davidign1688@hotmail.com>
2018-01-04 15:17:05 +0100

    Modificación en las rutas

--- App/Routes.php
+++ App/Routes.php
@@ -4,20 +4,26 @@ $app->group('/layndon', function (){
 
 	$this->get('', 'LayndonController:home')->setName('home');
 
-	$this->group('/login', function (){
-
-		$this->get('', 'LayndonController:login')->setName('login');
-		$this->post('', 'LayndonController:check')->setName('check');
-	});
+	// $this->group('/login', function (){
+		
+		
+	// });
 
 	$this->group('/admin', function (){
 
-		$this->get('', 'LayndonController:adminProfile')->setName('admin');
-		$this->post('', 'LayndonController:addFilm')->setName('add-film');
-		$this->get('/{id}', 'LayndonController:deleteFilm')->setName('delete-film');
-	});
-});
+		$this->get('', 'LayndonController:login')->setName('login');
+		$this->post('', 'LayndonController:check')->setName('check');
 
+		$this->group('/dashboard',function (){
+			$this->get('', 'LayndonController:adminProfile')->setName('admin');
+			$this->post('', 'LayndonController:addFilm')->setName('add-film');
+			$this->get('/delete/{id}', 'LayndonController:deleteFilm')->setName('delete-film');
+			$this->get('/edit/{id}', 'LayndonController:editFilm')->setName('edit-film');
+		});
+	});
+
+	$this->get('/film/{id}','LayndonController:detailsFilm')->setName('details-film');
+});
 
 
 
