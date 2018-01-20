41e0268fdc427f6f63b7d82d8ecc7f9f6a8dbccb
Davidfhk <davidign1688@hotmail.com>
2018-01-20 21:26:29 +0100

    Archivo js del admin, aqui estaran las peticiones ajax

---
+++ App/layndon/js/admin/admin.js
@@ -0,0 +1,29 @@
+$('.delete-image').click(function(e){
+  
+  var id_film = e.target.id
+  var host = window.location;
+  
+  var endpoint = host + '/delete/' + id_film;
+
+  $.ajax({	
+	url: endpoint,
+	type:'DELETE'	  
+  }).done( function() {
+    	window.location.reload()
+	})
+});
+
+$('.see-image').click(function(e){
+  
+  var id_film = e.target.id
+  var host = 'http://localhost/layndon-v2/public/layndon'
+  
+  var endpoint = host + '/film/' + id_film;
+
+  $.ajax({	
+	url: endpoint,
+	type:'GET'	  
+  }).done( function() {
+    	location = endpoint;
+	})
+})
