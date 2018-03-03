c5136e3355aaddf1cec22a864d8bba767beb4949
Davidfhk <davidign1688@hotmail.com>
2018-03-03 03:19:22 +0100

    Modificación en el ajax

--- App/layndon/js/admin/admin.js
+++ App/layndon/js/admin/admin.js
@@ -1,29 +1,33 @@
-$('.delete-image').click(function(e){
-  
-  var id_film = e.target.id
-  var host = window.location;
-  
-  var endpoint = host + '/delete/' + id_film;
 
-  $.ajax({	
-	url: endpoint,
-	type:'DELETE'	  
-  }).done( function() {
-    	$('.'+id_film).remove();
-	})
-});
 
-$('.see-image').click(function(e){
+  $('.delete-image').click(function(e){
   
-  var id_film = e.target.id
-  var host = 'http://localhost/layndon-v2/public/layndon'
-  
-  var endpoint = host + '/film/' + id_film;
+    var id_film = e.target.id
+    var host = window.location;
+    
+    var endpoint = host + '/delete/' + id_film;
 
-  $.ajax({	
-	url: endpoint,
-	type:'GET'	  
-  }).done( function() {
-    	location = endpoint;
-	})
-})
+    $.ajax({  
+    url: endpoint,
+    type:'DELETE'   
+    }).done( function() {
+        $('.'+id_film).remove();
+    })
+  });
+
+  $('.see-image').click(function(e){
+    
+    var id_film = e.target.id
+    var host = 'http://localhost/layndon-v2/public/layndon'
+    
+    var endpoint = host + '/film/' + id_film;
+
+    $.ajax({  
+    url: endpoint,
+    type:'GET'    
+    }).done( function() {
+        location = endpoint;
+    })
+  })
+
+
